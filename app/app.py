ef475356bd9e57ab2615cc8042eeba28e010a519
fdehouck <fdehouck@gmail.com>
2021-07-26 15:37:37 +0200

    test_a_propos_page_link_6

--- app/app.py
+++ app/app.py
@@ -3,12 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def indexPage():
     return render_template('index.html')
 	
 @app.route('/apropos')
-def hello2():
+def aProposPage():
     return render_template('apropos.html')
 
+@app.route('/enquete')
+def enquetePage():
+	return render_template('enquete.html')
+
+@app.route('/contact')
+def contactPage():
+	return render_template('contact.html')
+	
+	
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
