d35046b92defa76dade8ccb5f2f50ed70a0cf3bd
fdehouck <fdehouck@gmail.com>
2021-10-25 11:53:12 +0200

    webiste_mobile_v3_bothversions

--- app/app.py
+++ app/app.py
@@ -5,16 +5,16 @@ from wtforms.fields.html5 import EmailField
 from flask_mail import Mail, Message
 import os
 
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
+#from azure.keyvault.secrets import SecretClient
+#from azure.identity import DefaultAzureCredential
 
-credential =  DefaultAzureCredential()
-keyVaultName = os.environ["KEY_VAULT_NAME"]
-client     =  SecretClient(vault_url=f"https://{keyVaultName}.vault.azure.net/", credential=credential)
+#credential =  DefaultAzureCredential()
+#keyVaultName = os.environ["KEY_VAULT_NAME"]
+#client     =  SecretClient(vault_url=f"https://{keyVaultName}.vault.azure.net/", credential=credential)
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY']= client.get_secret('passwordcfr').value
+app.config['SECRET_KEY']= 'beuh1002' #client.get_secret('passwordcfr').value
 
 
 class contactForm(FlaskForm):
@@ -31,7 +31,7 @@ app.config.update(dict(
     MAIL_USE_TLS = True,
     MAIL_USE_SSL = False,
     MAIL_USERNAME = 'transitionalimentairebe@gmail.com',
-    MAIL_PASSWORD = client.get_secret('passwordmail').value,
+    MAIL_PASSWORD = 'beuh1002' # client.get_secret('passwordmail').value,
 ))
 
 
@@ -75,5 +75,39 @@ def questionnaire():
     return render_template('questionnaire.html')
 
 
+@app.route('/old', methods=["GET","POST"])
+def indexPage2():
+
+    form = contactForm()
+    name = False
+
+    if form.validate_on_submit():
+        name = form.name.data
+        email= form.adresseMail.data
+        codepost = form.codePost.data
+        messageM = form.textMess.data
+
+
+        newline= '\n'
+
+        msg = Message(f"Vous avez reçu un nouveau message de {name.upper()}", sender =   'transitionalimentairebe@gmail.com', recipients = ['transitionalimentairebe@gmail.com'])
+        msg.body = f"This is a message from {name}, code postal {codepost}, email {email} with message {newline}{newline} {messageM}"
+        mail.send(msg)
+
+        flash("ton message a bien été envoyé. Merci a toi.")
+
+        form.name.data = ''
+        form.name.data = ''
+        form.adresseMail.data = ''
+        form.codePost.data = ''
+        form.textMess.data = ''
+
+        submission_successful = True #or False. you can determine this.
+        return redirect(url_for('indexPage2', _anchor="contactFormFinal" ))
+
+
+    return render_template('index2.html', form=form, name=name)
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8082, debug=True)
